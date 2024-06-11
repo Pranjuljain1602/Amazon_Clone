7ca7e62e5a2387c7eacf034ce11e071429246e4c
Pranjuljain1602 <pranjuljain54@gmail.com>
2024-06-12 00:52:34 +0530

    Completed

---
+++ src/Firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDDpi3BV6VV3nProI2zprtWZ-87l3XUY-w",
+  authDomain: "e-clone-207b0.firebaseapp.com",
+  projectId: "e-clone-207b0",
+  storageBucket: "e-clone-207b0.appspot.com",
+  messagingSenderId: "286186497624",
+  appId: "1:286186497624:web:b2ae01d809594a013a8c2d",
+  measurementId: "G-Y3W3HFVRH9"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
